b7401d5e984a8c62c15053a8d67ebfdcb3fa3caa
Gabrielmou <gabrielmoura98@gmail.com>
2018-08-16 13:24:53 -0300

    Proj2

---
+++ Projeto 2 - Client Server/interface.py
@@ -0,0 +1,35 @@
+import tkinter as tk
+from tkinter.filedialog import askopenfilename
+
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.pack()
+        self.create_widgets()
+
+    def create_widgets(self):
+        
+        self.selectbutton = tk.Button(self, text="Selecionar imagem", fg="black",
+                              command=self.selectimage)
+
+        self.selectbutton.pack(side="top")              
+
+        self.sendbutton = tk.Button(self, text="Enviar imagem", fg="black",
+                              command=self.sendimage)
+        self.sendbutton.pack(side="top")
+
+        self.quit = tk.Button(self, text="Fechar janela", fg="black",
+                              command=root.destroy)
+        self.quit.pack(side="bottom")
+
+    def selectimage(self):
+        self.filename = askopenfilename() 
+        print("Imagem selecionada")   
+        
+    def sendimage(self):
+        print("Mensagem enviada")
+ 
+
+root = tk.Tk()
+app = Application(master=root)
+app.mainloop()
